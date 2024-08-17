5e51aa6576a6ae92824933d4f1920d6cd9179f4b
jax-01 <jrcastillo6@up.edu.ph>
2024-08-17 18:46:37 +0800

    Define shiftRight method in ArrayElementShift class

--- src/ArrayElementShift.java
+++ src/ArrayElementShift.java
@@ -1,10 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ArrayElementShift {
     /*
      *  input array: [1, 2, 3, 4, 5]
      *  number of steps: 3
      * output array: [3, 4, 5, 1, 2]
      */
-    public void shiftRight(int[] arr, int n) {
+    public void shiftRight() {
+        Scanner in = new Scanner(System.in);
+        int[] arr = Arrays.stream(in.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int steps = in.nextInt();
 
+        for (int i = 0; i < steps; i++) {
+            int temp = arr[arr.length - 1];
+            for (int j = arr.length - 1; j > 0; j--) {
+                arr[j] = arr[j - 1];
+            }
+            arr[0] = temp;
+        }
+        System.out.print("Shifted array: ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
     }
 }
