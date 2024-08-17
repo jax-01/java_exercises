20e17250e6190e6d78cd602ea610e11e9ed0e4d0
jax-01 <jrcastillo6@up.edu.ph>
2024-08-17 18:48:09 +0800

    Initialize an ArrayElementShift object

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        PrintStarFrom2DArray star = new PrintStarFrom2DArray();
-        star.printStar();
+//        PrintStarFrom2DArray star = new PrintStarFrom2DArray();
+//        star.printStar();
+
+        ArrayElementShift shift = new ArrayElementShift();
+        shift.shiftRight();
     }
 }
