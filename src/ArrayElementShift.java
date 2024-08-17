dac0868decbc016d3f5492d35999053c263273a4
jax-01 <jrcastillo6@up.edu.ph>
2024-08-17 18:12:08 +0800

    Create ArrayElementShift class

---
+++ src/ArrayElementShift.java
@@ -0,0 +1,10 @@
+public class ArrayElementShift {
+    /*
+     *  input array: [1, 2, 3, 4, 5]
+     *  number of steps: 3
+     * output array: [3, 4, 5, 1, 2]
+     */
+    public void shiftRight(int[] arr, int n) {
+
+    }
+}
