87e96fc8d1c7f5e7b87a2162c72fb414026f101c
jax-01 <jrcastillo6@up.edu.ph>
2024-08-19 21:07:20 +0800

    Define shiftLeft method in ArrayElementShift class

--- src/ArrayElementShift.java
+++ src/ArrayElementShift.java
@@ -24,4 +24,23 @@ public class ArrayElementShift {
             System.out.print(i + " ");
         }
     }
+
+    public void shiftLeft() {
+        Scanner in = new Scanner(System.in);
+        int[] arr = Arrays.stream(in.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int steps = in.nextInt();
+
+        for (int i = 0; i < steps; i++) {
+            int temp = arr[0];
+            for (int j = 0; j < arr.length - 2; j++) {
+                arr[j] = arr[j + 1];
+            }
+            arr[0] = temp;
+        }
+
+        System.out.print("Shifted array: ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
 }
