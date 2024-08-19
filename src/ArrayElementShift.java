986aa5df681f74dfe86c13492bb6a5598517d831
jax-01 <jrcastillo6@up.edu.ph>
2024-08-19 21:14:39 +0800

    Change upper boundary of loop in shiftLeft method

--- src/ArrayElementShift.java
+++ src/ArrayElementShift.java
@@ -32,10 +32,10 @@ public class ArrayElementShift {
 
         for (int i = 0; i < steps; i++) {
             int temp = arr[0];
-            for (int j = 0; j < arr.length - 2; j++) {
+            for (int j = 0; j < arr.length - 1; j++) {
                 arr[j] = arr[j + 1];
             }
-            arr[0] = temp;
+            arr[arr.length - 1] = temp;
         }
 
         System.out.print("Shifted array: ");
