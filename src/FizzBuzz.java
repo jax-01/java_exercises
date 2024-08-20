cbf3ce74f803fce70d4dd05bf3c6874d3e8b8a46
jax-01 <jrcastillo6@up.edu.ph>
2024-08-20 10:25:16 +0800

    Declare isDivByFive to check if a number is divisible by five

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,4 +2,8 @@ public class FizzBuzz {
     public static boolean isDivByThree(int num) {
         return num % 3 == 0;
     }
+
+    public static boolean isDivByFive(int num) {
+        return num % 5 == 0;
+    }
 }
