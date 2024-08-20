899ca8aa7247c9ab97dbaaa45d8c102b08fca202
jax-01 <jrcastillo6@up.edu.ph>
2024-08-20 10:29:39 +0800

    Create test case for FizzBuzz class

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,17 @@ public class Main {
 //        PrintStarFrom2DArray star = new PrintStarFrom2DArray();
 //        star.printStar();
 
-        ArrayElementShift shift = new ArrayElementShift();
-        shift.shiftRight();
+//        ArrayElementShift shift = new ArrayElementShift();
+//        shift.shiftRight();
+
+//        if (FizzBuzz.isDivByThreeAndFive(15)) {
+//            System.out.println("FizzBuzz");
+//        } else if (FizzBuzz.isDivByThree(15)) {
+//            System.out.println("Fizz");
+//        } else if (FizzBuzz.isDivByFive(15)) {
+//            System.out.println("Buzz");
+//        } else {
+//            System.out.println("None");
+//        }
     }
 }
