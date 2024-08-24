fe6e8e0dd184eb4bd230e131c549daffb8db6446
jax-01 <jrcastillo6@up.edu.ph>
2024-08-24 10:48:34 +0800

    Create doubleCharacters method for duplicating every element in a string

--- src/DoubleCharacters.java
+++ src/DoubleCharacters.java
@@ -1,3 +1,15 @@
-public class DoubleCharacters {
+import java.util.Scanner;
 
+public class DoubleCharacters {
+    public void doubleCharacters() {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String newStr = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            newStr += Character.toString(str.charAt(i)) + Character.toString(str.charAt(i));
+        }
+
+        System.out.println(newStr);
+    }
 }
