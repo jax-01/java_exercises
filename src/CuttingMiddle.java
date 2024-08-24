a0fad13bb4611e7080ac2de086619583286cb5c6
jax-01 <jrcastillo6@up.edu.ph>
2024-08-24 18:39:16 +0800

    Create method for removing the middle character(s) of a string

--- src/CuttingMiddle.java
+++ src/CuttingMiddle.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class CuttingMiddle {
+
+    public static void cutMiddle() {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String strOutput = "";
+
+        // even
+        if (str.length() % 2 == 0) {
+            strOutput += str.substring(0, str.length() / 2 - 1) + str.substring(str.length() / 2 + 1);
+        } else {
+            strOutput += str.substring(0, str.length() / 2) + str.substring(str.length() / 2 + 1);
+        }
+        System.out.println(strOutput);
+        sc.close();
+    }
 }
