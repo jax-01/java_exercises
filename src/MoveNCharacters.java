2aa58e8710148cc1407e2a27f13171f072f94254
jax-01 <jrcastillo6@up.edu.ph>
2024-08-24 11:11:02 +0800

    Create method for moving n number of characters in a string to the back

--- src/MoveNCharacters.java
+++ src/MoveNCharacters.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class MoveNCharacters {
+
+    public void moveNCharacters() {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = sc.nextInt();
+
+        System.out.println(n > str.length() ? str : str.substring(n) + str.substring(0, n));
+        sc.close();
+    }
 }
