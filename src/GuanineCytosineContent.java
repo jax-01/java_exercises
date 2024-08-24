5cdfa6cd012bcbd6b49a4c3ada422aa6adac41bd
jax-01 <jrcastillo6@up.edu.ph>
2024-08-24 11:32:40 +0800

    Define method to calculate the percent content of Guanine and Cytosine in a genome

--- src/GuanineCytosineContent.java
+++ src/GuanineCytosineContent.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class GuanineCytosineContent {
+
+    public static void calculatePercentGC() {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine().toLowerCase();
+        int count = 0;
+
+        for (char ch : input.toCharArray()) {
+            if (ch == 'c' || ch == 'g') {
+                count++;
+            }
+        }
+        double res = (double) count / input.length();
+        System.out.printf("%.1f", res * 100);
+        sc.close();
+    }
 }
