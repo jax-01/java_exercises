092d5e250ea7ec0eb5f5d7e7587823afc970141a
jax-01 <jrcastillo6@up.edu.ph>
2024-08-24 14:11:41 +0800

    Create method for checking palindrome

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class Palindrome {
+    public static void main(String[] args) {
+        checkPalindrome();
+    }
+
+    public static void checkPalindrome() {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                isPalindrome = false;
+            }
+        }
+
+        System.out.println(isPalindrome ? "yes" : "no");
+        sc.close();
+    }
 }
