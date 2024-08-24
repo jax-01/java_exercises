b89148133e5351fb30c60724a70ae6ed34df7a54
jax-01 <jrcastillo6@up.edu.ph>
2024-08-24 20:13:08 +0800

    Create function for filling a given n x n matrix

--- src/MatrixDiagonalFill.java
+++ src/MatrixDiagonalFill.java
@@ -1,2 +1,21 @@
 public class MatrixDiagonalFill {
+
+    public static void diagonalFill() {
+        int n = new java.util.Scanner(System.in).nextInt();
+        int[][] matrix = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                // value of each cell is the distance from the main diagonal
+                matrix[i][j] = Math.abs(i - j);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
