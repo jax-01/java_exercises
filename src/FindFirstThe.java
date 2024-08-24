2cc33eb4c24f5b61fd7f503840bd77bd49e21052
jax-01 <jrcastillo6@up.edu.ph>
2024-08-24 13:22:04 +0800

    Create method for finding first occurence of a given word

--- src/FindFirstThe.java
+++ src/FindFirstThe.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class FindFirstThe {
+    public static void main(String[] args) {
+        findFirstOccurence();
+    }
+
+    public static void findFirstOccurence() {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine().toLowerCase();
+
+        if (!str.contains("the")) {
+            System.out.println(-1);
+            return;
+        }
+        System.out.println(str.indexOf("the"));
+        sc.close();
+    }
 }
