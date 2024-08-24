92c4400bb56fc5aaf73c25fa1b555889565bf310
jax-01 <jrcastillo6@up.edu.ph>
2024-08-24 11:56:25 +0800

    Create method for finding the longest sentence in a given sentence

--- src/LongestSentence.java
+++ src/LongestSentence.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class LongestSentence {
+
+    public static void findLongestSentence() {
+        Scanner sc = new Scanner(System.in);
+        String sentence = sc.nextLine();
+        String[] words = sentence.split(" ");
+        String longest = "";
+
+        for (String word : words) {
+            if (word.length() > longest.length()) {
+                longest = word;
+            }
+        }
+
+        System.out.println(longest);
+        sc.close();
+    }
 }
