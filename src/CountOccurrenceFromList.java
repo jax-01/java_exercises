060cf1f8239d14d6e923ef99f24a6a46667b42a1
jax-01 <jrcastillo6@up.edu.ph>
2024-09-09 14:24:35 +0800

    Declare a variable that will store 0 if the value occurs the same number of times in the two lists

--- src/CountOccurrenceFromList.java
+++ src/CountOccurrenceFromList.java
@@ -2,6 +2,7 @@ import java.util.List;
 
 public class CountOccurrenceFromList {
     public static boolean checkOccurrence(int value, List<Integer> list1, List<Integer> list2) {
+        int check = 0;
 
         return false;
     }
