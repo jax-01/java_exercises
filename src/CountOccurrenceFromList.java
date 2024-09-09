00fc304b7a80a3d66a96dacb19215ba3645ec557
jax-01 <jrcastillo6@up.edu.ph>
2024-09-09 14:32:35 +0800

    Revert "Check each integer in the first list if it equals the value being searched"
    This reverts commit f92c0196c1285a8c6ef8e625e7c03b5ad8e8a880.

--- src/CountOccurrenceFromList.java
+++ src/CountOccurrenceFromList.java
@@ -3,12 +3,6 @@ import java.util.List;
 public class CountOccurrenceFromList {
     public static boolean checkOccurrence(int value, List<Integer> list1, List<Integer> list2) {
 
-        for (Integer num : list1) {
-            if (num == value) {
-
-            }
-        }
-
         return false;
     }
 }
