f8f99be5e24b797784a313b0e5cb3ab69a9aec24
jax-01 <jrcastillo6@up.edu.ph>
2024-09-09 14:37:11 +0800

    Check the occurrence of value in the list

--- src/CountOccurrenceFromList.java
+++ src/CountOccurrenceFromList.java
@@ -2,6 +2,13 @@ import java.util.List;
 
 public class CountOccurrenceFromList {
     public static boolean checkOccurrence(int value, List<Integer> list1, List<Integer> list2) {
+        int check = 0;
+
+        for (Integer num : list1) {
+            if (num == value) {
+                check++;
+            }
+        }
 
         return false;
     }
