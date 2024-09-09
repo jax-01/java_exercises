de5a894265d01e3edb323aad083cc8923c9c3f90
jax-01 <jrcastillo6@up.edu.ph>
2024-09-09 14:21:52 +0800

    Create method for checking the occurrence of value in two lists

--- src/CountOccurrenceFromList.java
+++ src/CountOccurrenceFromList.java
@@ -1,2 +1,8 @@
+import java.util.List;
+
 public class CountOccurrenceFromList {
+    public static boolean checkOccurrence(int value, List<Integer> list1, List<Integer> list2) {
+
+        return false;
+    }
 }
