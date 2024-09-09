f92c0196c1285a8c6ef8e625e7c03b5ad8e8a880
jax-01 <jrcastillo6@up.edu.ph>
2024-09-09 14:26:56 +0800

    Check each integer in the first list if it equals the value being searched

--- src/CountOccurrenceFromList.java
+++ src/CountOccurrenceFromList.java
@@ -4,6 +4,12 @@ public class CountOccurrenceFromList {
     public static boolean checkOccurrence(int value, List<Integer> list1, List<Integer> list2) {
         int check = 0;
 
+        for (Integer num : list1) {
+            if (num == value) {
+
+            }
+        }
+
         return false;
     }
 }
